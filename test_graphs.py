cacbd4581fbe4d2abd52ed12f3f2ad6a40b51ffd
Noah Kaplan <noahkaplan4@gmail.com>
2016-06-14 14:59:09 -0400

    building tests

--- test_graphs.py
+++ test_graphs.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 
 class Node:
 
@@ -21,9 +23,10 @@ def build_unweighted_undirected_graph(nodes, edges):
 
     graph = {node: [] for node in nodes}
 
-    for node_a, node_b in edges:
-        graph[node_a].append(node_b)
-        graph[node_b].append(node_a)
+    for node_1, node_2, weight in edges:
+        for node_a, node_b in ((node_1, node_2), (node_2, node_1)):
+            if node_b not in graph[node_a]:
+                graph[node_a].append(node_b)
 
     return graph
 
@@ -32,11 +35,17 @@ def build_unweighted_undirected_colored_graph(labels, edges):
 
     graph = [Node(label) for label in labels]
 
-    for labels in edges:
-        for node in graph:
-            if node.label in labels:
-                neighbor = labels[0] if labels[0] != node.label else labels[1]
-                node.neighbors.append(neighbor)
+    for label_1, label_2, weight in edges:
+        for label_a, label_b in ((label_1, label_2), (label_2, label_1)):
+
+            for node in graph:
+                if node.label == label_a:
+                    out_node = node
+                if node.label == label_b:
+                    in_node = node
+
+            if in_node not in out_node.neighbors:
+                out_node.neighbors.append(in_node)
 
     return graph
 
@@ -48,15 +57,63 @@ graph_types = {
 }
 
 
-test_graphs = {graph_type: {} for graph_type in graph_types}
+test_graphs = defaultdict(dict)
+
+shortest_paths = {}
 
 
-def build_graphs(test_name, nodes, edges):
+nodes = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+
+test = None # test name
+
+
+def build_graphs(edges):
     for graph_type, build_graph_type in graph_types.iteritems():
-        test_graphs[graph_type][test_name] = build_graph_type(nodes, edges)
+        test_graphs[test][graph_type] = build_graph_type(nodes, edges)
 
 
-# disconnected
+def insert_shortest_path_tests(start_node, target_node, first_shortest_path, second_shortest_path):
+    shortest_paths[test] = {
+        1: (start_node, target_node, first_shortest_path),
+        2: (start_node, target_node, second_shortest_path),
+    }
 
-nodes = ['A', 'B', 'C', 'D', 'E']
-build_graphs('disconnected', nodes, [])
+
+test = 'disconnected'
+
+build_graphs([])
+insert_shortest_path_tests('A', 'D', None, None)
+
+
+test = 'leaf'
+
+build_graphs([('A', 'B', 1), ('A', 'C', 4), ('B', 'D', 2), ('C', 'E', 3), ('D', 'F', 7),
+              ('E', 'G', 1)])
+insert_shortest_path_tests('A', 'D', ['A', 'B', 'D'], None)
+
+
+test = 'tree'
+
+build_graphs([('A', 'B', 2), ('A', 'C', 9), ('B', 'D', 3), ('B', 'E', 1), ('C', 'F', 4),
+              ('E', 'G', 2)])
+insert_shortest_path_tests('A', 'G', ['A', 'B', 'E', 'G'], None)
+
+
+test = 'cycle'
+
+build_graphs([('A', 'B', 6), ('B', 'C', 9), ('C', 'D', 4), ('D', 'E', 7), ('E', 'F', 4),
+              ('F', 'G', 1), ('G', 'A', 9)])
+insert_shortest_path_tests('B', 'D', ['B', 'C', 'D'], None)
+
+
+test = 'cyclic'
+
+build_graphs([('A', 'B', 1), ('A', 'C', 3), ('A', 'D', 7), ('B', 'C', 3), ('C', 'A', 9),
+              ('D', 'E', 4), ('D', 'F', 5), ('E', 'F', 3), ('F', 'E', 2), ('F', 'G', 9)])
+insert_shortest_path_tests('A', 'F', ['A', 'D', 'F'], ['A', 'D', 'E', 'F'])
+
+
+test = 'loop'
+
+build_graphs([('A', 'B', 4), ('B', 'B', 5), ('B', 'C', 9)])
+insert_shortest_path_tests('A', 'C', ['A', 'B', 'C'], ['A', 'B', 'B', 'C'])
